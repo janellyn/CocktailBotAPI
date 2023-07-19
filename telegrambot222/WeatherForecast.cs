89e05dcc65c4558d048d2d2f79c4c837078f7471
Zhenya Zhehlova <102433656+janellyn@users.noreply.github.com>
2023-07-19 19:49:12 +0300

    Update

--- telegrambot222/WeatherForecast.cs
+++ telegrambot222/WeatherForecast.cs
@@ -1,13 +1,1 @@
-namespace telegrambot222
-{
-    public class WeatherForecast
-    {
-        public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
-    }
-}
